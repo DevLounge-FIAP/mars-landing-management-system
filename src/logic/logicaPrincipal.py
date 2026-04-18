9a2d5171bc5a3450e5752395d7d4a800aca232c0
Bruno-S-Martins <RM572073@fiap.com.br>
2026-04-17 21:03:32 -0300

    Colocando Nome nos modulos

--- src/logic/logicaPrincipal.py
+++ src/logic/logicaPrincipal.py
@@ -7,9 +7,9 @@ massaDaNave = 21000
 # Tabela de Verificação de segurança
 print("=-=-=-=-=- VERIFICAÇÃO DE SEGURANÇA =-=-=-=-=-")
 if combustivel < 25:
-    print(f"{'Combustível':<25} | {combustivel:^10} | {'ALERTA!':^10}")
+    print(f"{'Combustível':<25} | {f'{combustivel} %':^10} | {'ALERTA!':^10}")
 else:
-    print(f"{'Combustível':<25} | {combustivel:^10} | {'OK!':^10}")
+    print(f"{'Combustível':<25} | {f'{combustivel} %':^10} | {'OK!':^10}")
 if areaDePouso != True:
     print(f"{'Local de Pouso':<25} | {areaDePouso:^10} | {'ALERTA!':^10}")
 else:
@@ -18,10 +18,10 @@ if criticidade < 4:
     print(f"{'Criticidade da Nave':<25} | {criticidade:^10} | {'ALERTA!':^10}")
 else:
     print(f"{'Criticidade da Nave':<25} | {criticidade:^10} | {'OK!':^10}")
-if massaDaNave:
-    print(f"{'Massa da Nave':<25} | {massaDaNave:^10} | {'ALERTA!':^10}")
+if massaDaNave < 21000:
+    print(f"{'Massa da Nave':<25} | {f'{massaDaNave} Kg':^10} | {'ALERTA!':^10}")
 else:
-    print(f"{'Massa da Nave':<25} | {massaDaNave:^10} | {'OK!':^10}")
+    print(f"{'Massa da Nave':<25} | {f'{massaDaNave} Kg':^10} | {'OK!':^10}")
 print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
 # Sistema de autoização do Pouso de acordo com os parâmetros fornecidos
