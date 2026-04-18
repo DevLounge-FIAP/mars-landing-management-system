e700d8f081b183ef18b9cf70c2b26d598f9aaa87
Bruno-S-Martins <RM572073@fiap.com.br>
2026-04-17 22:41:29 -0300

    Finalizando...

--- src/logic/logicaPrincipal.py
+++ src/logic/logicaPrincipal.py
@@ -1,31 +1,44 @@
-combustivel = 70
-areaDePouso = True
-criticidade = 4
-massaDaNave = 21000
+A = combustivel = 25
+B = areaDePouso = True
+C = criticidade = 4
+D = massaDaNave = 90000
 
 
 # Tabela de Verificação de segurança
 print("=-=-=-=-=- VERIFICAÇÃO DE SEGURANÇA GERAL DOS MÓDULOS =-=-=-=-=-")
-if combustivel < 25:
-    print(f"{'Combustível':<25} | {f'{combustivel} %':^10} | {'ALERTA!':^10}")
+if A < 25:
+    print(f"{'Combustível':<25} | {f'{A} %':^10} | {'CRÍTICO!':^10}")
+elif A < 50:
+    print(f"{'Combustível':<25} | {f'{A} %':^10} | {'ALERTA!':^10}")
 else:
-    print(f"{'Combustível':<25} | {f'{combustivel} %':^10} | {'OK!':^10}")
-if areaDePouso != True:
-    print(f"{'Local de Pouso':<25} | {areaDePouso:^10} | {'ALERTA!':^10}")
+    print(f"{'Combustível':<25} | {f'{A} %':^10} | {'OK!':^10}")
+if B != True:
+    print(f"{'Local de Pouso':<25} | {B:^10} | {'CRÍTICO!':^10}")
 else:
-    print(f"{'Local de Pouso':<25} | {areaDePouso:^10} | {'OK!':^10}")
-if criticidade < 4:
-    print(f"{'Criticidade da Nave':<25} | {criticidade:^10} | {'ALERTA!':^10}")
+    print(f"{'Local de Pouso':<25} | {B:^10} | {'OK!':^10}")
+if C < 3:
+    print(f"{'Criticidade da Nave':<25} | {C:^10} | {'CRÍTICO!':^10}")
+elif C == 3:
+    print(f"{'Criticidade da Nave':<25} | {C:^10} | {'ALERTA!':^10}")
 else:
-    print(f"{'Criticidade da Nave':<25} | {criticidade:^10} | {'OK!':^10}")
-if massaDaNave < 21000:
-    print(f"{'Massa da Nave':<25} | {f'{massaDaNave} Kg':^10} | {'ALERTA!':^10}")
+    print(f"{'Criticidade da Nave':<25} | {C:^10} | {'OK!':^10}")
+if D < 70000:
+    print(f"{'Massa da Nave':<25} | {f'{D} Kg':^10} | {'CRÍTICO!':^10}")
+elif D < 90000:
+    print(f"{'Massa da Nave':<25} | {f'{D} Kg':^10} | {'ALERTA!':^10}")
 else:
-    print(f"{'Massa da Nave':<25} | {f'{massaDaNave} Kg':^10} | {'OK!':^10}")
+    print(f"{'Massa da Nave':<25} | {f'{D} Kg':^10} | {'OK!':^10}")
 print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 
 # Sistema de autoização do Pouso de acordo com os parâmetros fornecidos
-if combustivel >= 25 and areaDePouso == True:
+if A >= 50 and B == True and D >= 90000 and C > 3:
     print("\nPouso AUTORIZADO!")
+elif (A >= 25 or C == 3 or D < 90000) and B == True:
+    print("\nPouso AUTORIZADO com RISCO!")
+    if A >= 25:
+        print(f"Combustivel da Nave: {A}%")
+    elif C == 3:
+        print(f"Criticidade da Nave: {C}")
 else:
-    print("\nPouso NÃO AUTORIZADO!")
+    print("\nMissão Abortada!")
+    print("Pouso NÃO AUTORIZADO!")
